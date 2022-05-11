12275a536e1c7d2f3bf0e282d169e3db8bb84a46
Soohyun Lee <84074670+topnotchsh@users.noreply.github.com>
2022-05-11 15:55:22 +0900

    220511

---
+++ Assets/UI/SoundManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+	public AudioSource musicsource;
+	public AudioSource btnsource;
+
+    public void SetMusicVolume(float volume){
+    	musicsource.volume = volume;
+    }
+
+    public void SetBtnVolume(float volume){
+    	btnsource.volume = volume;
+    }
+
+    public void OnSfx(){
+    	btnsource.Play();
+    }
+}
