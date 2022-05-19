85cc8b89813a41b02ef19ce9dcf89e5ca89f897c
topnotchsh <dltngus00@ewhain.net>
2022-05-20 05:07:44 +0900

    220520 인트로 구현

---
+++ Assets/UI/IntroToStart.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroToStart : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("GotoStart", 6f);
+    }
+
+    // Update is called once per frame
+    void GotoStart()
+    {
+    	SceneManager.LoadScene(1);
+    }
+}
