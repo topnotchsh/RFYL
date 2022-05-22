7c5f603d5a30864fe191707db4c2cce8308c40f9
DayeonKang <dayeon0914@ewhain.net>
2022-05-23 01:08:35 +0900

    door animation & finished prefabs

--- Assets/Scripts/DoorOpen.cs
+++ Assets/Scripts/DoorOpen.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class DoorOpen : MonoBehaviour
 {
+    Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    void OnTriggerEnter(Colider other){
+        if(other.tag == 'Player'){
+            animator.SetBool("IsOpen", true);
+        }
     }
 }
