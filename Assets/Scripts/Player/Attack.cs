7c56ffc52ad32255f033bc35eb2bb6d8304eebfa
btothey99@ewhain.net <btothey99@ewhain.net>
2022-06-03 23:03:59 +0900

    220603 player attack
    + 타겟 공격시 없어짐
    + 공격 애니메이션
    - 공격 애니메이션이 되돌아오지가 않음...

---
+++ Assets/Scripts/Player/Attack.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    public Animator anim;
+    public GameObject target;
+    // Start is called before the first frame update
+
+    void Awake(){
+        anim = gameObject.GetComponent<Animator>();
+        anim.SetBool("IsAttack", false);
+    }
+
+    public void PlayerAttack(){
+        // 귀신과 마주치면
+
+        // 타겟 이미지를 n초 안에 클릭했으면
+        //if (Input.GetButtonDown("Fire1"))
+        
+        anim.SetBool("IsAttack", true);
+        //SoundManager.instance.AttackSound();
+        Destroy(target);
+        
+    }
+
+    public void PlayerAttacked() {
+        anim.SetBool("IsAttack", false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
