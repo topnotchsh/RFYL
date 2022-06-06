f6d85a3834c235077b0bdeee23f4841eff428ee5
topnotchsh <dltngus00@ewhain.net>
2022-06-07 04:06:57 +0900

    220607 미니맵 완성+UI 작업 조금

---
+++ Assets/UI/minimap/mapReveal.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mapReveal : MonoBehaviour
+{
+    // Update is called once per frame
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+            Debug.Log("접촉");
+        }
+    }
+}
